36c34c178b5621a45135b26a605b34e96002cb0a
pratpat <33582382+pratpat@users.noreply.github.com>
2023-08-23 14:51:40 -0400

    Update __init__.py

--- api/Python/Pib/__init__.py
+++ api/Python/Pib/__init__.py
@@ -47,7 +47,7 @@ def processStep1(pibIndexName, cik, step, symbol, temperature, llm, today):
         sData = {
                 'id' : str(uuid.uuid4()),
                 'symbol': symbol,
-                'cik': cik,
+                'cik': str(cik),
                 'step': step,
                 'description': 'Company Profile',
                 'insertedDate': today.strftime("%Y-%m-%d"),
